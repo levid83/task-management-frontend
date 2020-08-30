cb972f1c26b59566bd8f1c0f879211472e62b65e
Levente Danel <danellevente@yahoo.com>
2020-08-30 17:17:00 +0300

    implement a page for task creation

---
+++ src/pages/create-task/CreateTaskPage.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { TextField, FormControl, Button } from "@material-ui/core";
+import styled from "styled-components";
+
+const FormWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FormContainer = styled.div`
+  max-width: 480px;
+  width: 100%;
+  background-color: #edf4ff;
+  padding: 30px;
+  border-radius: 5px;
+`;
+
+const CreateTaskPage = (props: any) => {
+  const [, setTitle] = useState("");
+  const [, setDescription] = useState("");
+
+  const handleSubmitTask = async () => {};
+
+  return (
+    <FormWrapper>
+      <FormContainer>
+        <h1>Create a new task</h1>
+        <FormControl fullWidth>
+          <TextField
+            label="Title"
+            placeholder="Title"
+            margin="normal"
+            variant="outlined"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <TextField
+            label="Description"
+            placeholder="Description"
+            multiline
+            rows="8"
+            margin="normal"
+            variant="outlined"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </FormControl>
+        <Button
+          style={{ marginTop: "10px" }}
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={handleSubmitTask}
+        >
+          CREATE TASK
+        </Button>
+      </FormContainer>
+    </FormWrapper>
+  );
+};
+export default CreateTaskPage;
